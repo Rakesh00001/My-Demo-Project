4ef94ef9a89c2ea9cb4e2f25db7eef4bf5fdc44c
Rakesh Mandal <rm811283@gmai.com>
2022-02-23 15:43:49 +0530

    My Demo project no-4

---
+++ main.js
@@ -0,0 +1,9 @@
+var xyz = document.getElementById('navLink');
+showMenu = () => {
+    xyz.style.right = '0px';
+    xyz.style.transition = '1s';
+}
+hideMenu = () =>{
+    xyz.style.right = '-350px';
+    xyz.style.transition = '1s';
+}
